ec68032585a3f1117cab4040a90f9f0f4fc3f319
candramuftir <candramuftiramadhan25@gmail.com>
2025-06-29 03:06:33 +0700

    Initial upload of Ujikom project

---
+++ footer.php
@@ -0,0 +1,11 @@
+<footer id="footer">
+
+            <!-- FOOTER BOTTOM -->
+            <div class="footer_bottom">
+                <div class="container">
+                    <p>&copy; <?php echo date('Y');?> ATRIA Hotels All rights reserved | Developed By <a href="http://candramf.com/" target="_blank" style="color: #fff;">Candra Mufti Ramadhan</a> </p>
+                </div>
+            </div>
+            <!-- END / FOOTER BOTTOM -->
+
+        </footer>
